9915784c4d93398643ba596f1d752dde6776fed0
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2021-02-06 20:37:00 +0200

    first commit

---
+++ acordion.js
@@ -0,0 +1,16 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("activeAcordion");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      panel.style.padding = "0";
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.padding = "10px 0";
+    }
+  });
+}
